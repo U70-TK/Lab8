c0815941a65efa20c66d66910ed6402785051bd2
Wang <u7095437@anu.edu.au>
2023-03-10 14:22:54 -0700

    Added DeleteCity test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -33,4 +33,13 @@ public class CustomListTest {
         assertEquals(true,list.hasCity(city));
     }
 
+    @Test
+    public void testDeleteCity(){
+        CustomList list = new CustomList(null, citylist);
+        City city = new City("Edmonton","AB");
+        list.addCity(city);
+        assertEquals(1,list.getCount());
+        list.deleteCity(city);
+        assertEquals(0,list.getCount());
+    }
 }
